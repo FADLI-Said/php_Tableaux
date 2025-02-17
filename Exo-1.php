407688c8e6528fc0856f7213505c88bdc2c81776
FADLI-Said <saidfadli224@gmail.com>
2025-02-17 16:23:38 +0100

    Correction

--- Exo-1.php
+++ Exo-1.php
@@ -1,8 +1,13 @@
 <?php 
 
-$name = ["Saïd", "Alice", "Bob", "Charlie", "David", "Ichem"];
+$name = ["Alice", "Bob", "Charlie", "David", "Emma"];
+
+array_push($name,"Ichem");
+array_unshift($name,"Saïd");
 
 var_dump($name);
+
+unset($name[1]);
 
 foreach ($name as $i => $value) {
     echo $name[$i] . ", ";
